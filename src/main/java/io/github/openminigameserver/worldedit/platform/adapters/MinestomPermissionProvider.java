3affef3747c4ae2efc071147d01ff943358b1fc7
Window5 <13127531-Window5@users.noreply.gitlab.com>
2022-12-10 16:49:04 +0100

    add command condition

--- src/main/java/io/github/openminigameserver/worldedit/platform/adapters/MinestomPermissionProvider.java
+++ src/main/java/io/github/openminigameserver/worldedit/platform/adapters/MinestomPermissionProvider.java
@@ -4,13 +4,13 @@ import net.minestom.server.entity.Player;
 
 public class MinestomPermissionProvider {
     public static boolean hasPermission(Player player, String permission) {
-        if (player.hasPermission("*") || player.hasPermission("worldedit.*"))
+        if (player.hasPermission("*") || player.hasPermission("worldedit.*") || player.getPermissionLevel() >= 4)
             return true;
         return player.hasPermission(permission);
     }
 
     public static boolean hasWorldEditPermission(Player player) {
-        return player.hasPermission("*") || player.hasPermission("worldedit");
+        return player.hasPermission("*") || player.hasPermission("worldedit") || player.getPermissionLevel() >= 4;
     }
 
 }
